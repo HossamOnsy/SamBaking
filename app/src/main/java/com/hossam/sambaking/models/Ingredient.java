26df9a1388fa038c06fe2e46f7b68c079da05be5
HossamEldeen <HossamEldeenOnsy@yahoo.com>
2017-11-24 18:35:02 +0200

    Initial commit

---
+++ app/src/main/java/com/hossam/sambaking/models/Ingredient.java
@@ -0,0 +1,47 @@
+package com.hossam.sambaking.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by hossam on 17/11/17.
+ */
+
+
+public class Ingredient {
+
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("measure")
+    @Expose
+    private String measure;
+    @SerializedName("ingredient")
+    @Expose
+    private String ingredient;
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getMeasure() {
+        return measure;
+    }
+
+    public void setMeasure(String measure) {
+        this.measure = measure;
+    }
+
+    public String getIngredient() {
+        return ingredient;
+    }
+
+    public void setIngredient(String ingredient) {
+        this.ingredient = ingredient;
+    }
+
+}
